2802883254c4f8e405733fb7d0961aca5cb61b6a
TianyAI <106868269+TianyAI@users.noreply.github.com>
2023-05-05 23:03:45 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,9 +96,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT For Tianyai</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          请畅言.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
